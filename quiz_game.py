e02908f6693c5a54ca6469fbf07bd6ebdcdb1d27
zackcarlson014 <jzcarlson014@gmail.com>
2025-02-23 17:22:20 -0600

    additional questions, modified incorrect response messages

--- quiz_game.py
+++ quiz_game.py
@@ -18,11 +18,25 @@ if answer.lower() == "central processing unit":
     print("Correct!")
     score += 1
 else:
-    print("Incorrect!")
+    print("Incorrect! The correct answer is central processing unit.")
 
 answer = input("What does GPU stand for? ")
 if answer.lower() == "graphics processing unit":
     print("Correct!")
     score += 1
 else:
-    print("Incorrect!")
+    print("Incorrect! The correct answer is graphics processing unit.")
+
+answer = input("What does RAM stand for? ")
+if answer.lower() == "random access memory":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect! The correct answer is random access memory.")
+
+answer = input("What does PSU stand for? ")
+if answer.lower() == "power supply unit":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect! The correct answer is power supply unit.")
