5e2e604a0eee6f3a95847d6816398cf6ba83d539
zackcarlson014 <jzcarlson014@gmail.com>
2025-02-23 17:20:51 -0600

    instructions, score vairable created, first two questions

--- quiz_game.py
+++ quiz_game.py
@@ -4,3 +4,25 @@ playing = input("Do you want to play? (yes/no) ")
 
 print(playing)
 print("Okay! Let's play!")
+
+if playing.lower() != "yes":
+    quit()
+
+print("I will ask you a series of questions.")
+print("You will answer them.")
+
+score = 0
+
+answer = input("What does CPU stand for? ")
+if answer.lower() == "central processing unit":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What does GPU stand for? ")
+if answer.lower() == "graphics processing unit":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
