111d9e29219bdfc1f59ed66647c9cd7bd91a47ae
zackcarlson014 <jzcarlson014@gmail.com>
2025-02-23 17:31:31 -0600

    input prompt for number guesser created

--- number_guesser.py
+++ number_guesser.py
@@ -1,3 +1,11 @@
 import random
 
 print("Welcome to Z's number guesser game!")
+
+playing = input("Do you want to play? (yes/no) ")
+
+if playing.lower() != "yes":
+    quit()
+
+print("Okay! Let's play!")
+
