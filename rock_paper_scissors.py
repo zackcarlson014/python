ca8f3b6896c3c2c7cb93b4544d01b69c44cc5c18
zackcarlson014 <jzcarlson014@gmail.com>
2025-02-23 18:18:52 -0600

    handle user wins

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -16,4 +16,13 @@ while True:
 
     if user_input == computer_pick:
         print("It's a tie!")
+    elif user_input == "rock" and computer_pick == "scissors":
+        print("You won!")
+        user_wins += 1
+    elif user_input == "paper" and computer_pick == "rock":
+        print("You won!")
+        user_wins += 1
+    elif user_input == "scissors" and computer_pick == "paper":
+        print("You won!")
+        user_wins += 1
     
