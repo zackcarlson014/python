dc630a6273ec203935605d723b80123018feb2e5
zackcarlson014 <jzcarlson014@gmail.com>
2025-02-23 18:18:16 -0600

    handle computer pick

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -8,4 +8,10 @@ while True:
     user_input = input("Type Rock/Paper/Scissors or Q to quit: ").lower()
     if user_input == "q":
         break
+    if user_input not in options:
+        continue # This will skip the rest of the code in the loop and start from the beginning
+    random_number = random.randint(0, 2)
+    computer_pick = options[random_number]
+    print("Computer picked", computer_pick + ".")
+
     
