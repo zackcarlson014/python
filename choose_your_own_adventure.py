76881b462ea0bde9afdcadbdbdf665636a03ab56
zackcarlson014 <jzcarlson014@gmail.com>
2025-02-23 18:22:53 -0600

    choose your own adventure first scenario

--- choose_your_own_adventure.py
+++ choose_your_own_adventure.py
@@ -1,5 +1,8 @@
 name = input("What is your name? ")
 print("Hello, " + name + "!")
 print("Welcome to Z's choose your own adventure game!")
-
+print("You are on a deserted island.")
+print("You see a treasure chest.")
+print("Do you want to open it?")
+choice = input("Yes/No: ").lower()
 
