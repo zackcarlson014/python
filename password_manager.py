ef8a4a4b862b388d218f6855a3ad4183c1b801fe
zackcarlson014 <jzcarlson014@gmail.com>
2025-02-24 07:47:50 -0600

    handle mode input

--- password_manager.py
+++ password_manager.py
@@ -1,3 +1,16 @@
 pwd = input("What is the master passwrord? ")
 
 mode = input("Do you want to add a new password or retrieve existing ones (add/view)? ")
+
+if mode == "add":
+    new_pwd = input("What is the new password? ")
+    with open("passwords.txt", "a") as f:
+        f.write(f"{pwd} {new_pwd}\n")
+elif mode == "view":
+    with open("passwords.txt", "r") as f:
+        for line in f:
+            master_pwd, pwd = line.split()
+            if master_pwd == pwd:
+                print(pwd)
+else:
+    print("Invalid mode.")
