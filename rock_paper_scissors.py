f699d338a48ae3311bb9d10196fcc9086d4d338e
zackcarlson014 <jzcarlson014@gmail.com>
2025-02-23 18:17:56 -0600

    handle user quit

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -3,3 +3,9 @@ import random
 user_wins = 0
 computer_wins = 0
 options = ["rock", "paper", "scissors"]
+
+while True:
+    user_input = input("Type Rock/Paper/Scissors or Q to quit: ").lower()
+    if user_input == "q":
+        break
+    
