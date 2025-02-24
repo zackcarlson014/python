148bc3160d0c2398ad4687aa086699e45e8516b1
zackcarlson014 <jzcarlson014@gmail.com>
2025-02-23 18:19:31 -0600

    print scores and goodbye message

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -29,4 +29,6 @@ while True:
         print("You lost!")
         computer_wins += 1
 
-
+print("You won", user_wins, "times.")
+print("The computer won", computer_wins, "times.")
+print("Goodbye!")
