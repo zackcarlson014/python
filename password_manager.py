f01d47faa12076ae7c78d5f048c948a14777e8ca
zackcarlson014 <jzcarlson014@gmail.com>
2025-02-25 11:21:34 -0600

    password manager updates

--- password_manager.py
+++ password_manager.py
@@ -1,16 +1,28 @@
 pwd = input("What is the master passwrord? ")
 
-mode = input("Do you want to add a new password or retrieve existing ones (add/view)? ")
+mode = input("Do you want to add a new password or retrieve existing ones (add/view/quit)? ")
 
-if mode == "add":
-    new_pwd = input("What is the new password? ")
-    with open("passwords.txt", "a") as f:
-        f.write(f"{pwd} {new_pwd}\n")
-elif mode == "view":
-    with open("passwords.txt", "r") as f:
-        for line in f:
-            master_pwd, pwd = line.split()
-            if master_pwd == pwd:
-                print(pwd)
-else:
-    print("Invalid mode.")
+while True:
+    if mode == "quit":
+        quit()
+    elif mode == "add" or mode == "view":
+        pass
+    else:
+        mode = input("Invalid mode. Do you want to add a new password or retrieve existing ones (add/view)? ")
+        continue
+
+# if mode == "add":
+#     new_pwd = input("What is the new password? ")
+#     with open("passwords.txt", "a") as f:
+#         f.write(f"{pwd} {new_pwd}\n")
+# elif mode == "view":
+#     with open("passwords.txt", "r") as f:
+#         for line in f:
+#             master_pwd, pwd = line.split()
+#             if master_pwd == pwd:
+#                 print(pwd)
+# else:
+#     print("Invalid mode")
+
+
+
