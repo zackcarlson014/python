038b9a242bcb4b79a5046648446c8d3cf467e38f
zackcarlson014 <jzcarlson014@gmail.com>
2025-02-25 11:34:48 -0600

    removed redundant code

--- password_manager.py
+++ password_manager.py
@@ -25,18 +25,6 @@ while True:
         mode = input("Invalid mode. Do you want to add a new password or retrieve existing ones (add/view)? ")
         continue
 
-# if mode == "add":
-#     new_pwd = input("What is the new password? ")
-#     with open("passwords.txt", "a") as f:
-#         f.write(f"{pwd} {new_pwd}\n")
-# elif mode == "view":
-#     with open("passwords.txt", "r") as f:
-#         for line in f:
-#             master_pwd, pwd = line.split()
-#             if master_pwd == pwd:
-#                 print(pwd)
-# else:
-#     print("Invalid mode")
 
 
 
