5f41c9487a71ae64f0889c5df15af220db10758a
zackcarlson014 <jzcarlson014@gmail.com>
2025-02-27 18:45:47 -0600

    commented out cryptography

--- password_manager.py
+++ password_manager.py
@@ -1,4 +1,13 @@
+# from cryptography.fernet import Fernet
+
 master_pwd = input("What is the master passwrord? ")
+
+# def write_key():
+#   key = Fernet.generate_key()
+#   with open("key.key", "wb") as key_file:
+#     key_file.write(key)
+
+# write_key()
 
 def view():
     with open("passwords.txt", "r") as f:
