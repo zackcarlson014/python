8d10699ed12c88cddeda9a60d5e60161e78d2248
zackcarlson014 <jzcarlson014@gmail.com>
2025-03-13 07:35:04 -0500

    generate problem function created

--- timed_math_challenge.py
+++ timed_math_challenge.py
@@ -5,3 +5,12 @@ OPERATORS = ["+", "-", "*"]
 MIN_OPERAND = 3
 MAX_OPERAND = 12
 TOTAL_PROBLEMS = 10
+
+def generate_problem():
+    left = random.randint(MIN_OPERAND, MAX_OPERAND)
+    right = random.randint(MIN_OPERAND, MAX_OPERAND)
+    operator = random.choice(OPERATORS)
+
+    expr = str(left) + " " + operator + " " + str(right)
+    answer = eval(expr)
+    return expr, answer
