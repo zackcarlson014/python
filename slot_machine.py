fcbd0cb7a5721bdef8146934cdfc23e9e2ba0a9c
zackcarlson014 <jzcarlson014@gmail.com>
2025-03-16 20:04:18 -0500

    check winnings function created

--- slot_machine.py
+++ slot_machine.py
@@ -20,3 +20,18 @@ symbol_value = {
     "C": 3,
     "D": 2
 }
+
+def check_winnings(columns, lines, bet, values):
+    winnings = 0
+    winning_lines = []
+    for line in range(lines):
+        symbol = columns[0][line]
+        for column in columns:
+            symbol_to_check = column[line]
+            if symbol != symbol_to_check:
+                break
+        else:
+            winnings += values[symbol] * bet
+            winning_lines.append(line + 1)
+
+    return winnings, winning_lines
