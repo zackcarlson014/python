bf81df27b13f701bb3267da9752f94ee4abb5076
zackcarlson014 <jzcarlson014@gmail.com>
2025-03-16 20:06:06 -0500

    get number of lines function created

--- slot_machine.py
+++ slot_machine.py
@@ -78,3 +78,18 @@ def deposit():
             print("Please enter a number.")
 
     return amount
+
+def get_number_of_lines():
+    while True:
+        lines = input(
+            "Enter the number of lines to bet on (1-" + str(MAX_LINES) + ")? ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print("Enter a valid number of lines.")
+        else:
+            print("Please enter a number.")
+
+    return lines
