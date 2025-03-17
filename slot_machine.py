c5ddc8003ed8d862a87763db9b1020e8137fe773
zackcarlson014 <jzcarlson014@gmail.com>
2025-03-16 20:04:43 -0500

    slot machine spin function created

--- slot_machine.py
+++ slot_machine.py
@@ -35,3 +35,22 @@ def check_winnings(columns, lines, bet, values):
             winning_lines.append(line + 1)
 
     return winnings, winning_lines
+
+def get_slot_machine_spin(rows, cols, symbols):
+    all_symbols = []
+    for symbol, symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+
+    columns = []
+    for _ in range(cols):
+        column = []
+        current_symbols = all_symbols[:]
+        for _ in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+
+        columns.append(column)
+
+    return columns
