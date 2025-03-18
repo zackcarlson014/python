022b955c65479b6b9fd8e52e64032c54b68e63eb
zackcarlson014 <jzcarlson014@gmail.com>
2025-03-18 08:13:40 -0500

    main function created

--- shortest_path_finder.py
+++ shortest_path_finder.py
@@ -82,3 +82,10 @@ def find_neighbors(maze, row, col):
         neighbors.append((row, col + 1))
 
     return neighbors
+
+def main(stdscr):
+    curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+
+    find_path(maze, stdscr)
+    stdscr.getch()
