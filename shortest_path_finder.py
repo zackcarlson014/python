fbc1d69712cd7cfe28c18143de94bc22eb6470f0
zackcarlson014 <jzcarlson014@gmail.com>
2025-03-18 08:13:13 -0500

    find neighbors function created

--- shortest_path_finder.py
+++ shortest_path_finder.py
@@ -68,3 +68,17 @@ def find_path(maze, stdscr):
             new_path = path + [neighbor]
             q.put((neighbor, new_path))
             visited.add(neighbor)
+
+def find_neighbors(maze, row, col):
+    neighbors = []
+
+    if row > 0:  # UP
+        neighbors.append((row - 1, col))
+    if row + 1 < len(maze):  # DOWN
+        neighbors.append((row + 1, col))
+    if col > 0:  # LEFT
+        neighbors.append((row, col - 1))
+    if col + 1 < len(maze[0]):  # RIGHT
+        neighbors.append((row, col + 1))
+
+    return neighbors
