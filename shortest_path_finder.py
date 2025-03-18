aea41189debd9be522abc10d39317d14f9a05622
zackcarlson014 <jzcarlson014@gmail.com>
2025-03-18 08:12:07 -0500

    print maze function created

--- shortest_path_finder.py
+++ shortest_path_finder.py
@@ -14,3 +14,14 @@ maze = [
     ["#", " ", " ", " ", " ", " ", " ", " ", "#"],
     ["#", "#", "#", "#", "#", "#", "#", "X", "#"]
 ]
+
+def print_maze(maze, stdscr, path=[]):
+    BLUE = curses.color_pair(1)
+    RED = curses.color_pair(2)
+
+    for i, row in enumerate(maze):
+        for j, value in enumerate(row):
+            if (i, j) in path:
+                stdscr.addstr(i, j*2, "X", RED)
+            else:
+                stdscr.addstr(i, j*2, value, BLUE)
