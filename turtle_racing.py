682badf02eb941e652af70e09c2a7f15cdf322fc
zackcarlson014 <jzcarlson014@gmail.com>
2025-03-23 19:52:42 -0500

    get number of racers function created

--- turtle_racing.py
+++ turtle_racing.py
@@ -4,3 +4,18 @@ import random
 
 WIDTH, HEIGHT = 700, 600
 COLORS = ['red', 'green', 'blue', 'orange', 'yellow', 'black', 'purple', 'pink', 'brown', 'cyan']
+
+def get_number_of_racers():
+	racers = 0
+	while True:
+		racers = input('Enter the number of racers (2 - 10): ')
+		if racers.isdigit():
+			racers = int(racers)
+		else:
+			print('Input is not numeric... Try Again!')
+			continue
+
+		if 2 <= racers <= 10:
+			return racers
+		else:
+			print('Number not in range 2-10. Try Again!')
